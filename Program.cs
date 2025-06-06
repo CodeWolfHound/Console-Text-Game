45306a80bd2d31c0de325ac7d1793182c9974e49
Reeni Rivera <159098065+CodeWolfHound@users.noreply.github.com>
2025-06-05 23:49:48 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.CompilerServices;
 public class Game
 {
@@ -17,6 +17,14 @@ public class Game
     {
         Game Intro = new Game();
         Intro.GameIntro();
+        if (Intro.isPlaying)
+        {
+            Intro.LevelOne();
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
     }
     /// <summary>
     /// Displays entry game intro context and modifys a boolean variable
@@ -31,7 +39,8 @@ public class Game
         switch (answer)
         {
             case "y":
-                Console.WriteLine("Great lets get started.");
+                Console.Write("Great lets get started.");
+                Console.ReadLine();
                 isPlaying = true;
                 break;
             case "n":
@@ -42,18 +51,22 @@ public class Game
                 break;
         }
     }
+    public void LevelManager()
+    {
+        Console.WriteLine("This is a test");
+    }
+    public void LevelOne()
+    {
+        Console.WriteLine("Henry needs to take a path , theres a path 1 and path 2 ");
+        Console.ReadLine();
+        Console.WriteLine("Path 1 : Theres a long narrow road paved in gold ");
+        Console.ReadLine();
+        Console.WriteLine("Path 2 : A tunnel that leads under a mountain , ominous and cold");
+        Console.ReadLine();
+        Console.Write("What Path would you like to take?: ");
+        Console.ReadLine();
+
+    }
     #endregion
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
